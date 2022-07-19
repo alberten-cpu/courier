8bf9cc2c6714c9789ed10ccfb5adbc5abdc7f92c
unknown <alberteb2910@gmail.com>
2022-07-19 10:59:39 +0530

    edit_job

--- app/Http/Controllers/Admin/JobController.php
+++ app/Http/Controllers/Admin/JobController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Job;
+use DataTables;
 use App\Models\AddressBook;
 use App\Models\DailyJob;
 use Illuminate\Contracts\Foundation\Application;
@@ -26,6 +27,7 @@ class JobController extends Controller
      */
     public function index()
     {
+        return view('template.admin.job.edit_job');
     }
 
     /**
